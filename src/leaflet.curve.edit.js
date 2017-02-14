1683fe68a5c178d7496b82d5dd22b4f8c8205bcd
Ethan McCoy <SirenHound@users.noreply.github.com>
2017-02-14 13:59:03 +0800

    getLatLngs
    Added getLatLngs function, note it does not specify if they are a control or point on the curve yet.

---
+++ src/leaflet.curve.edit.js
@@ -0,0 +1,5 @@
+var Curve = require('./leaflet.curve.js');
+
+Curve.include({
+  getLatLngs: function(){ return this._coords.filter(function(coord){return Array.isArray(coord);}).map(function(coord){return L.latLng(coord);});}
+});
